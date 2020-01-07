6504775babcecefeb365dfd1f9514042f99faa2d
Cody Morgan <cody.morgan@digipen.edu>
2020-01-06 17:07:08 -0800

    triangle drawing

--- CS300/Assn1/CS300/Source/Object.cpp
+++ CS300/Assn1/CS300/Source/Object.cpp
@@ -151,6 +151,9 @@ ObjectManager::~ObjectManager()
 
 void ObjectManager::render()
 {
+  glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
   for(Object obj : objects_)
   {
     obj.draw();
