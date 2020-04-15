378ffca81ec455b28e4d7ca3a3fc9dceb22c841e
codymorg <codymorg@gmail.com>
2020-04-14 18:27:45 -0700

    bash

--- CS330/bash.sh
+++ CS330/bash.sh
@@ -6,11 +6,12 @@ size=$(ls | grep -wc out*.*)
 for ((i = 0 ; i <= $size ; i++)); do
   echo
   echo
-  echo test $i 
+  echo test $i
+  rm myout$i
   time ./gnu.exe $i > myout$i
   echo end test ==========================
-  echo diff ==============================
-  diff myout$i out$i
+  echo diff  [mine]  [target]=============
+  diff myout$i out$i --strip-trailing-cr -y --suppress-common-lines
   echo end diff ==========================
   
 done
