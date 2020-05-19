fd348eab29ffd46cbc72f18b38ebb3036daac5c9
codymorg <codymorg@gmail.com>
2020-05-19 14:10:34 -0400

    island hopper program

---
+++ SoT_IslandMapper/IslandBox.cpp
@@ -0,0 +1,15 @@
+#include "IslandBox.h"
+
+
+bool IslandBox::isInBox(unsigned xPos, unsigned yPos) const
+{
+  return xPos >= x && xPos <= x + w && yPos >= y && yPos <= y + h;
+}
+
+void IslandBox::draw(const unsigned char* color, CImg<unsigned char>& image)
+{
+  image.draw_line(x, y, x + w, y, color, 1); // TL->TR
+  image.draw_line(x + w, y, x + w, y + h, color, 1); // TR_>BR 
+  image.draw_line(x, y, x, y + h, color, 1); // TL->BL
+  image.draw_line(x, y + h, x + w, y + h, color, 1); // BL->BR
+}
---
+++ SoT_IslandMapper/IslandBox.h
@@ -0,0 +1,22 @@
+#ifndef _ISLANDBOX_
+#define _ISLANDBOX_
+#pragma once
+
+#include "CImg-2.9.1_pre042420/CImg.h"
+using namespace cimg_library;
+
+class IslandBox
+{
+public:
+  IslandBox(unsigned xPos, unsigned yPos, unsigned width, unsigned height) : x(xPos), y(yPos), w(width), h(height) {};
+
+  bool isInBox(unsigned xPos, unsigned yPos) const;
+  void draw(const unsigned char* color, CImg<unsigned char>& image);
+
+  unsigned x;
+  unsigned y;
+  unsigned w;
+  unsigned h;
+};
+
+#endif
